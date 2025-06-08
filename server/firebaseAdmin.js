78ed1e47837dcd2aea2b80e3e49c684a28737c73
binuk <binukcodm@gmail.com>
2025-06-08 20:35:32 +0800

    LIVE CORS FIX 1.1

--- server/firebaseAdmin.js
+++ server/firebaseAdmin.js
@@ -1,6 +1,6 @@
 // server/firebaseAdmin.js
 const admin = require('firebase-admin');
-const serviceAccount = require('./serviceAccountKey.json'); // replace with your actual path
+const serviceAccount = require('/etc/secrets/serviceAccountKey.json'); // replace with your actual path
 
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount)
